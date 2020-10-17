09aca6a0dfe083c62857f01244feef42e412ce27
Nicolas Vairaa <vaira.nicolas@gmail.com>
2020-10-17 16:06:29 +0200

    Format files with prettier

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-const express = require('express');;
+const express = require('express');
 const app = express();
 const server = app.listen(8080);
 const io = require('socket.io')(server);
-import { generateNumberInRange } from './utils';
+import {generateNumberInRange} from './utils';
 
 const MIN_TIMER = 2;
 const MAX_TIMER = 6;
@@ -23,74 +23,74 @@ let lastPlayerReady = false;
 let signalShown = false;
 let fighterHasAttacked = false;
 
-io.on('connection', (socket) => {
-    if (firstPlayerConnected && lastPlayerConnected) {
-        return;
-    }
-    if (firstPlayerConnected === false) {
-        firstPlayerConnected = true;
-        socket.player = 1;
-        io.to(`${socket.id}`).emit('firstPlayer');
+io.on('connection', socket => {
+  if (firstPlayerConnected && lastPlayerConnected) {
+    return;
+  }
+  if (firstPlayerConnected === false) {
+    firstPlayerConnected = true;
+    socket.player = 1;
+    io.to(`${socket.id}`).emit('firstPlayer');
+  } else {
+    lastPlayerConnected = true;
+    socket.player = 2;
+    io.to(`${socket.id}`).emit('lastPlayer');
+  }
+  socket.join('players room');
+
+  socket.on('disconnect', () => {
+    if (socket.player === 1) {
+      firstPlayerConnected = false;
+      firstPlayerReady = false;
     } else {
-        lastPlayerConnected = true;
-        socket.player = 2;
-        io.to(`${socket.id}`).emit('lastPlayer');
+      lastPlayerConnected = false;
+      lastPlayerReady = false;
     }
-    socket.join('players room');
+    io.in('players room').emit('reset');
+  });
 
-    socket.on('disconnect', () => {
-        if (socket.player === 1) {
-            firstPlayerConnected = false;
-            firstPlayerReady = false;
+  socket.on('ready', () => {
+    if (socket.player === 1) {
+      firstPlayerReady = true;
+    } else if (socket.player === 2) {
+      lastPlayerReady = true;
+    }
+    if (firstPlayerReady && lastPlayerReady) {
+      fighterHasAttacked = false;
+      signalShown = false;
+      io.in('players room').emit('game-ready');
+      setTimeout(() => {
+        const allPlayersReady = firstPlayerReady === true && lastPlayerReady === true;
+        if (allPlayersReady && fighterHasAttacked === false && signalShown === false) {
+          io.in('players room').emit('signal');
+          signalShown = true;
         } else {
-            lastPlayerConnected = false;
-            lastPlayerReady = false;
+          signalShown = false;
         }
-        io.in('players room').emit('reset');
-    });
+      }, generateNumberInRange(MIN_TIMER, MAX_TIMER, 1000));
+    }
+  });
 
-    socket.on('ready', () => {
-        if (socket.player === 1) {
-            firstPlayerReady = true;
-        } else if (socket.player === 2){
-            lastPlayerReady = true;
-        }
-        if (firstPlayerReady && lastPlayerReady) {
-            fighterHasAttacked = false;
-            signalShown = false;
-            io.in('players room').emit('game-ready');
-            setTimeout(() => {
-                const allPlayersReady = firstPlayerReady === true && lastPlayerReady === true;
-                if (allPlayersReady && fighterHasAttacked === false && signalShown === false) {
-                    io.in('players room').emit('signal');
-                    signalShown = true;
-                } else {
-                    signalShown = false;
-                }
-            }, generateNumberInRange(MIN_TIMER, MAX_TIMER, 1000));
-        }
-    });
-
-    socket.on('attack', (timeToAttack) => {
-        if (fighterHasAttacked === true) {
-            return;
-        }
-        fighterHasAttacked = true;
-        let winner;
-        if (signalShown === false) {
-            winner = (socket.player === 1) ? 2 : 1;
-        } else {
-            winner = socket.player;
-        }
-        const nextBackground = generateNumberInRange(MIN_BG_ID, MAX_BG_ID);
-        io.in('players room').emit('endGame', {winner, nextBackground, timeToAttack});
-        resetGame();
-    });
+  socket.on('attack', timeToAttack => {
+    if (fighterHasAttacked === true) {
+      return;
+    }
+    fighterHasAttacked = true;
+    let winner;
+    if (signalShown === false) {
+      winner = socket.player === 1 ? 2 : 1;
+    } else {
+      winner = socket.player;
+    }
+    const nextBackground = generateNumberInRange(MIN_BG_ID, MAX_BG_ID);
+    io.in('players room').emit('endGame', {winner, nextBackground, timeToAttack});
+    resetGame();
+  });
 });
 
 function resetGame() {
-    fighterHasAttacked = false;
-    firstPlayerReady = false;
-    lastPlayerReady = false;
-    signalShown = false;
+  fighterHasAttacked = false;
+  firstPlayerReady = false;
+  lastPlayerReady = false;
+  signalShown = false;
 }
--- public/client.js
+++ public/client.js
@@ -8,7 +8,7 @@ let timer;
 let timeToAttack = 0;
 
 function getEl(identifier) {
-    return  document.querySelector(identifier);
+  return document.querySelector(identifier);
 }
 
 // HTML elements
@@ -27,182 +27,182 @@ const playerLabel = getEl('#player');
 
 // Socket events
 socket.on('reset', () => {
-    reset();
+  reset();
 });
 socket.on('game-ready', () => {
-    launchGame();
+  launchGame();
 });
 socket.on('firstPlayer', () => {
-    autoSelectFighter(1);
+  autoSelectFighter(1);
 });
 socket.on('lastPlayer', () => {
-    autoSelectFighter(2);
+  autoSelectFighter(2);
 });
-socket.on('endGame', (infos) => {
-    timeToAttack = (infos.timeToAttack / 1000).toFixed(3);
-    animateFighter(infos.winner, timeToAttack);
-    nextBackground = infos.nextBackground;
+socket.on('endGame', infos => {
+  timeToAttack = (infos.timeToAttack / 1000).toFixed(3);
+  animateFighter(infos.winner, timeToAttack);
+  nextBackground = infos.nextBackground;
 });
 socket.on('signal', showSignal);
 
 function showRules() {
-    modalRules.style.display = 'block';
+  modalRules.style.display = 'block';
 }
 
 function hideRules() {
-    modalRules.style.display = 'none';
+  modalRules.style.display = 'none';
 }
 
 // Game logic
 function autoSelectFighter(fighterId) {
-    if (fighterId === 1) {
-        player = 1;
-        sayanIndicator.classList.remove('hidden');
-        sayanIndicator.classList.remove('move');
-        sayanIndicator.classList.add('move');
-        playerLabel.innerHTML = 'You are ZACK';
-    } else {
-        player = 2;
-        ennemyIndicator.classList.remove('hidden');
-        ennemyIndicator.classList.remove('move');
-        ennemyIndicator.classList.add('move');
-        playerLabel.innerHTML = 'You are GRUNT';
-    }
+  if (fighterId === 1) {
+    player = 1;
+    sayanIndicator.classList.remove('hidden');
+    sayanIndicator.classList.remove('move');
+    sayanIndicator.classList.add('move');
+    playerLabel.innerHTML = 'You are ZACK';
+  } else {
+    player = 2;
+    ennemyIndicator.classList.remove('hidden');
+    ennemyIndicator.classList.remove('move');
+    ennemyIndicator.classList.add('move');
+    playerLabel.innerHTML = 'You are GRUNT';
+  }
 }
 
 function joinGame() {
-    hideBtnJoin();
-    waitOpponent();
-    socket.emit('ready', player);
+  hideBtnJoin();
+  waitOpponent();
+  socket.emit('ready', player);
 }
 
 function waitOpponent() {
-    gameIsReady = false;
-    signalShown = false;
-    hideSignal();
-    hideBtnJoin();
-    container.classList.remove('focus');
-    header.classList.remove('hidden');
-    header.classList.add('blink-opacity');
-    header.innerHTML = 'AWAITING THE OPPONENT...';
-    sayan.removeAttribute('style');
-    ennemy.removeAttribute('style');
+  gameIsReady = false;
+  signalShown = false;
+  hideSignal();
+  hideBtnJoin();
+  container.classList.remove('focus');
+  header.classList.remove('hidden');
+  header.classList.add('blink-opacity');
+  header.innerHTML = 'AWAITING THE OPPONENT...';
+  sayan.removeAttribute('style');
+  ennemy.removeAttribute('style');
 }
 
 function launchGame() {
-    gameIsReady = true;
-    header.classList.add('hidden');
-    container.classList.add('focus');
-    sayanIndicator.classList.remove('move');
-    ennemyIndicator.classList.remove('move');
-    hideBtnJoin();
-    prepareFighters();
-    'keyup touchend'.split(' ').forEach((eventName) => {
-        document.addEventListener(eventName, attack);
-    });
+  gameIsReady = true;
+  header.classList.add('hidden');
+  container.classList.add('focus');
+  sayanIndicator.classList.remove('move');
+  ennemyIndicator.classList.remove('move');
+  hideBtnJoin();
+  prepareFighters();
+  'keyup touchend'.split(' ').forEach(eventName => {
+    document.addEventListener(eventName, attack);
+  });
 }
 
 function showSignal() {
-    signalShown = true;
-    signal.style.visibility = 'visible';
-    timer = setInterval(() => {
-        timeToAttack += 1;
-    }, 1);
+  signalShown = true;
+  signal.style.visibility = 'visible';
+  timer = setInterval(() => {
+    timeToAttack += 1;
+  }, 1);
 }
 
 function hideSignal() {
-    signalShown = false;
-    signal.style.visibility = 'hidden';
+  signalShown = false;
+  signal.style.visibility = 'hidden';
 }
 
 function reset(_changeBackground) {
-    timeToAttack = 0;
-    clearInterval(timer);
-    if (_changeBackground === true) {
-        changeBackground(nextBackground);
-    } else {
-        container.removeAttribute('style');
-    }
-    header.removeAttribute('style');
-    header.classList.add('hidden');
-    container.classList.remove('focus');
-    sayan.removeAttribute('style');
-    ennemy.removeAttribute('style');
-    winnerMessage.classList.add('hidden');
-    btnRestart.classList.add('hidden');
-    gameIsReady = false;
-    showBtnJoin();
-    hideSignal();
-    autoSelectFighter(player);
+  timeToAttack = 0;
+  clearInterval(timer);
+  if (_changeBackground === true) {
+    changeBackground(nextBackground);
+  } else {
+    container.removeAttribute('style');
+  }
+  header.removeAttribute('style');
+  header.classList.add('hidden');
+  container.classList.remove('focus');
+  sayan.removeAttribute('style');
+  ennemy.removeAttribute('style');
+  winnerMessage.classList.add('hidden');
+  btnRestart.classList.add('hidden');
+  gameIsReady = false;
+  showBtnJoin();
+  hideSignal();
+  autoSelectFighter(player);
 }
 
 // Fighters logic
 function prepareFighters() {
-    sayan.style.backgroundPosition = '70.5% 0%';
-    ennemy.style.backgroundPosition = '0% -15.9%';
+  sayan.style.backgroundPosition = '70.5% 0%';
+  ennemy.style.backgroundPosition = '0% -15.9%';
 }
 
 function attack() {
-    if (gameIsReady === true) {
-        clearInterval(timer);
-        socket.emit('attack', timeToAttack);
-    }
+  if (gameIsReady === true) {
+    clearInterval(timer);
+    socket.emit('attack', timeToAttack);
+  }
 }
 
 function animateFighter(winner, timeToAttack) {
-    gameIsReady = false;
-    document.removeEventListener('keyup touchend', attack);
-    container.classList.remove('focus');
-    hideSignal();
-    container.style.animation = 'blink-opacity .5s linear';
-    if (winner === 1) {
-        sayanWin();
-    } else {
-        ennemyWin();
-    }
+  gameIsReady = false;
+  document.removeEventListener('keyup touchend', attack);
+  container.classList.remove('focus');
+  hideSignal();
+  container.style.animation = 'blink-opacity .5s linear';
+  if (winner === 1) {
+    sayanWin();
+  } else {
+    ennemyWin();
+  }
 
-    setTimeout(() => {
-        if (winner === 1) {
-            winnerMessage.innerHTML = `Winner ZACK !! <br>(${timeToAttack}s)`;
-        } else {
-            winnerMessage.innerHTML = `Winner GRUNT !! <br>(${timeToAttack}s)`;
-        }
-        winnerMessage.classList.remove('hidden');
-        btnRestart.classList.remove('hidden');
-    }, 800);
+  setTimeout(() => {
+    if (winner === 1) {
+      winnerMessage.innerHTML = `Winner ZACK !! <br>(${timeToAttack}s)`;
+    } else {
+      winnerMessage.innerHTML = `Winner GRUNT !! <br>(${timeToAttack}s)`;
+    }
+    winnerMessage.classList.remove('hidden');
+    btnRestart.classList.remove('hidden');
+  }, 800);
 }
 
 function sayanWin() {
-    const leftCoordEnnemy = ennemy.getBoundingClientRect().left + 30 + 'px';
-    sayan.style.backgroundPosition = '70.5% 13.1%';
-    sayan.style.left = leftCoordEnnemy;
-    ennemy.style.backgroundPosition = '40% -5.2%';
-    setTimeout(() => {
-        sayan.style.backgroundPosition = '23.5% 0.1%';
-        container.style.animation = 'none';
-    }, 500);
+  const leftCoordEnnemy = ennemy.getBoundingClientRect().left + 30 + 'px';
+  sayan.style.backgroundPosition = '70.5% 13.1%';
+  sayan.style.left = leftCoordEnnemy;
+  ennemy.style.backgroundPosition = '40% -5.2%';
+  setTimeout(() => {
+    sayan.style.backgroundPosition = '23.5% 0.1%';
+    container.style.animation = 'none';
+  }, 500);
 }
 
 function ennemyWin() {
-    const leftCoordSayan = sayan.getBoundingClientRect().right + 'px';
-    ennemy.style.backgroundPosition = '10% 68.4%';
-    ennemy.style.left = leftCoordSayan;
-    sayan.style.backgroundPosition = '146.5% 20%';
-    setTimeout(() => {
-        ennemy.style.backgroundPosition = '31% 78.9%';
-        container.style.animation = 'none';
-    }, 500);
+  const leftCoordSayan = sayan.getBoundingClientRect().right + 'px';
+  ennemy.style.backgroundPosition = '10% 68.4%';
+  ennemy.style.left = leftCoordSayan;
+  sayan.style.backgroundPosition = '146.5% 20%';
+  setTimeout(() => {
+    ennemy.style.backgroundPosition = '31% 78.9%';
+    container.style.animation = 'none';
+  }, 500);
 }
 
 // Display logic
 function hideBtnJoin() {
-    btnJoin.classList.add('hidden');
+  btnJoin.classList.add('hidden');
 }
 
 function showBtnJoin() {
-    btnJoin.classList.remove('hidden');
+  btnJoin.classList.remove('hidden');
 }
 
 function changeBackground(id) {
-    container.style.backgroundImage = `url('./img/bg-${id}.gif')`;
+  container.style.backgroundImage = `url('./img/bg-${id}.gif')`;
 }
